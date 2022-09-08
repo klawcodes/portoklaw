655e153f482e23c16b04459236348f15815100de
klawcodes <klawcodes@gmail.com>
2022-09-08 22:54:23 +0700

    ui done

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,16 +1,51 @@
 import React, { useEffect, useRef } from 'react'
 import './Works.css'
+import test from "../../images/mk.png"
 import { DiReact, DiHtml5, DiCss3, DiJsBadge, DiPython } from "react-icons/di"
 import { SiBlender, SiVuedotjs } from "react-icons/si";
+import { gsap } from 'gsap';
 import LocomotiveScroll from "locomotive-scroll";
 
 const Works = () => {
+
+  let tl = gsap.timeline();
+  let tl2 = gsap.timeline();
+  let cursor = useRef(null);
+  let h1 = useRef(null);
+  let posX = 0;
+  let posY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
+  useEffect(() => {
+    tl.to({},0.016, {
+      repeat: -1,
+      onRepeat: function(){
+        posX += (mouseX-posX) / 10;
+        posY += (mouseY-posY) / 10;
+        tl.set(cursor, {
+          css: {
+            left: posX - 50,
+            top: posY - 50,
+          }
+        })
+      }
+    })
+    document.addEventListener("mousemove", function(e){
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+    })
+    
+  })
+
   return (
     <div data-scroll>
       <div className="works">
         <h2>FEATURED WORKS.</h2>
+        {/*<div className="test">
+          <img src={test} className="cursor-follow"/>
+        </div>*/}
         <div className="list-works">
-          <div className="movie">
+          <div className="movie" >
             <div className="line">
               <h2 className="title">IMDB WANNA BE</h2>
               <DiReact title="React JS" className="icon"/>
