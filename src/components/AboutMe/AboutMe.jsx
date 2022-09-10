590401a82d0df574ecb9209ea48aad4e195a8005
klawcodes <codenamezeskil@gmail.com>
2022-09-10 16:37:40 +0700

    change percentage to image

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -32,102 +32,142 @@ I'm interested in programming since 2020, the first time i started programming w
         <h1>SKILLS.</h1>    
         <div className="skills">
           <div className="coding">
-            <h2>PROGRAMMING</h2>
+            <h2>PROGRAMMING LANGUAGES & TOOLS</h2>
             <div className="skill-main">
               <div className="skill-bar">
                 <div className="info">
-                  <p>HTML</p>
-                  <p>80%</p>
+                  {/*<p>HTML</p>
+                <p>80%</p>*/}
+                  <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="html"></span>
-                </div>
+              </div>*/}
               </div>
               <div className="skill-bar">
                 <div className="info">
-                  <p>CSS</p>
-                  <p>80%</p>
+                  {/*<p>CSS</p>
+                <p>80%</p>*/}
+                  <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="css"></span>
-                </div>
+              </div>*/}
               </div>
               <div className="skill-bar">
                 <div className="info">
-                  <p>JAVASCRIPT</p>
-                  <p>50%</p>
+                  {/*<p>JAVASCRIPT</p>
+                <p>50%</p>*/}
+                  <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="js"></span>
-                </div>
+              </div>*/}
               </div>
               <div className="skill-bar">
                 <div className="info">
-                  <p>REACT JS</p>
-                  <p>40%</p>
+                  {/*<p>REACT JS</p>
+                <p>40%</p>*/}
+                  <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="react"></span>
-                </div>
+                </div>*/}
               </div>
              <div className="skill-bar">
               <div className="info">
-                <p>VUE JS</p>
-                <p>40%</p>
+                {/*<p>VUE JS</p>
+              <p>40%</p>*/}
+                <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-plain.svg'/>
               </div>
-              <div className="bar">
+              {/*<div className="bar">
                 <span className="react"></span>
-              </div>
+            </div>*/}
             </div> 
               <div className="skill-bar">
                 <div className="info">
-                  <p>PYTHON</p>
-                  <p>30%</p>
+                  {/*<p>PYTHON</p>
+                <p>30%</p>*/}
+                  <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="python"></span>
+              </div>*/}
+              </div>
+              <div className="skill-bar">
+                <div className="info">
+                  {/*<p>PYTHON</p>
+                <p>30%</p>*/}
+                  <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg'/>
                 </div>
+                {/*<div className="bar">
+                  <span className="python"></span>
+              </div>*/}
+              </div>
+              <div className="skill-bar">
+                <div className="info">
+                  {/*<p>PYTHON</p>
+                <p>30%</p>*/}
+                  <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg'/>
+                </div>
+                {/*<div className="bar">
+                  <span className="python"></span>
+                </div>*/}
+              </div>
+              <div className="skill-bar">
+                <div className="info">
+                  {/*<p>PYTHON</p>
+                <p>30%</p>*/}
+                  <img className="prologo" src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg'/>
+                </div>
+                {/*<div className="bar">
+                  <span className="python"></span>
+              </div>*/}
               </div>
             </div>
           </div>
           <div className="design">
-            <h2>DESIGN</h2>
+            <h2>DESIGN TOOLS</h2>
             <div className="skill-main">
               <div className="skill-bar">
                 <div className="info">
-                  <p>ADOBE PHOTOSHOP</p>
-                  <p>90%</p>
+                  {/*<p>ADOBE PHOTOSHOP</p>
+                  <p>90%</p>*/}
+                  <img className="prologo" src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/photoshop/photoshop-plain.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="ps"></span>
-                </div>
+                </div>*/}
               </div>
                <div className="skill-bar">
                 <div className="info">
-                  <p>ADOBE AFTER EFFECT</p>
-                  <p>90%</p>
+                  {/*<p>ADOBE AFTER EFFECT</p>
+                  <p>90%</p>*/}
+                  <img className="prologo" src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/aftereffects/aftereffects-plain.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="ae"></span>
-                </div>
+                </div>*/}
               </div>
                <div className="skill-bar">
                 <div className="info">
-                  <p>BLENDER</p>
-                  <p>80%</p>
+                  {/*<p>BLENDER</p>
+                  <p>80%</p>*/}
+                  <img className="prologo" src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/blender/blender-original.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="blender"></span>
-                </div>
+                </div>*/}
               </div>
                <div className="skill-bar">
                 <div className="info">
-                  <p>ADOBE ILLUSTRATOR</p>
-                  <p>70%</p>
+                  {/*<p>ADOBE ILLUSTRATOR</p>
+                  <p>70%</p>*/}
+                  <img className="prologo" src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/illustrator/illustrator-plain.svg'/>
                 </div>
-                <div className="bar">
+                {/*<div className="bar">
                   <span className="ai"></span>
-                </div>
+                </div>*/}
               </div>
               
             </div>
